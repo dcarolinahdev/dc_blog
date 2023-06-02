5c23a74be811b35203c4264d9a1729950c365a41
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-06-02 18:56:54 -0500

    Testing connection with postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pblog_db',
+        'USER': 'admin_pblog',
+        'PASSWORD': 'adminblog1234.',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
