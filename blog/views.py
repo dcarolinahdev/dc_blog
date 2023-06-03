4b01637af1089141262fd29950e82a2afc59c923
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-03 18:39:52 -0500

    Feature: new post, Updating header and footer (#3)

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/index.html', {'posts': posts})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk) # to-do: template for 404
