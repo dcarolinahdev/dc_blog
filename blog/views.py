dbc67dabb256ef47ed5e1736d4c2e455caa629e4
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-06-03 16:15:39 -0500

    Starting with a basic template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.utils import timezone
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
