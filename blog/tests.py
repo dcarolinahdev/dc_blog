9d8c70f19823466dec3e837a55322e34cbbc8bd7
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-06-04 13:59:00 -0500

    More tests for function in model Post

--- blog/tests.py
+++ blog/tests.py
@@ -15,3 +15,22 @@ class PostModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_post = Post(published_date=time)
         self.assertIs(future_post.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_post(self):
+        """
+        was_published_recently() returns False for posts whose published_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_post = Post(published_date=time)
+        self.assertIs(old_post.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_recent_post(self):
+        """
+        was_published_recently() returns True for posts whose published_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_post = Post(published_date=time)
+        self.assertIs(recent_post.was_published_recently(), True)
