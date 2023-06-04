b24d5dbaa0074f1036dd3f835038d8d40253e62d
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-06-04 12:08:53 -0500

    Index view as class based view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django.views import generic
+
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
-def index(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now(), active=True).order_by('published_date')
-    return render(request, 'blog/index.html', {'posts': posts})
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now(), active=True).order_by('published_date')
 
 def post_new(request):
     if request.method == "POST":
