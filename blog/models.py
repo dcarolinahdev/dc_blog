e3842c61fb403dc7f258abf1b1355e6f36123786
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-06-03 20:40:39 -0500

    New model: comments

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
