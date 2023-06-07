5810fe6e2f0a94e22a067fc444a4b150bf3bb71c
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-06-07 12:12:02 -0500

    Updating styles v2

--- blog/views.py
+++ blog/views.py
@@ -74,7 +74,7 @@ def add_comment_to_post(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'post': post})
 
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
