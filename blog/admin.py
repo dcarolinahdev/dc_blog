121a05804034ef1137cd0b65488bb62694dff422
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2023-06-07 13:07:51 -0500

    Updating admin site for models Post and Comment (InLine)

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+# About Comment
+class CommentInline(admin.StackedInline):
+    model = Comment
+    fields = ["approved_comment", "text", "created_date"]
+    extra = 3
+
+# About Post
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Date information:", {
+            "fields":
+                ["created_date",
+                 "published_date"]
+        }),
+        ("Post information:", {
+            "fields":
+                ["title",
+                 "text",
+                 "active"]
+        }),
+    ]
+    inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
+
